714174127537db14c846ae2478fc0d98b3bf279e
Viollah <viollah0119@gmai.com>
2022-03-08 09:18:25 +0200

    first commit

---
+++ test/game.test.js
@@ -0,0 +1,29 @@
+
+describe('Guess a  number between 1 and 100', function () {
+    it("Higher,it has to be between 1 and 100", async function () {
+      
+    });
+    it("Lower, number should be between 1 and 100 ", async function () {
+     
+    });
+    
+    it("user should try again if the value is no correct", async function () {
+     
+    });
+    it(" you guessed right! ", async function () {
+     
+      
+    });
+  
+    it("should show 'higher' if the number guessed is greater than 100.", async function () {
+     
+    });
+    it("should show 'lower' if the number guessed is less than 1.", async function () {
+      
+    });
+  
+    it("should show message you guessed right! ", async function () {
+      
+    });
+  });
+  
